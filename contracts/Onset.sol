c9b5a523ab5765c7a7686b7fc8ff27af3fc8c445
vasa <vasa@signy.io>
2020-07-03 13:38:26 +0530

    refactor: add ability to add multiple items
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@dappkit.io>

--- contracts/Onset.sol
+++ contracts/Onset.sol
@@ -25,11 +25,6 @@ contract Onest {
         string qrcode;
     }
     
-    struct slice {
-        uint _len;
-        uint _ptr;
-    }
-    
     mapping (string => Product) public ProductsMapping;
     mapping (string => Item) public ItemsMapping;
     
@@ -47,8 +42,21 @@ contract Onest {
         ProductsMapping[productId] = Product(brand, name, Type, orgId, attributes);
     }
     
-    function addItems(string memory itemIds, uint256 scannedNos, string memory verificationUrls, string memory statuses, string memory soldTos, string memory soldLocations, string memory soldToUserIds) public {
+    function addItems(string memory itemIds, string memory verificationUrls) public {
+        strings.slice memory itemIdSlices = itemIds.toSlice();
+        strings.slice memory verificationUrlSlices = verificationUrls.toSlice();
+
+        strings.slice memory delim = ",".toSlice();
         
+        string[] memory itemIdArr = new string[](itemIdSlices.count(delim) + 1);
+        string[] memory verificationUrlArr = new string[](verificationUrlSlices.count(delim) + 1);
+        
+        for(uint i = 0; i < itemIdArr.length; i++) {
+            itemIdArr[i] = itemIdSlices.split(delim).toString();
+            verificationUrlArr[i] = verificationUrlSlices.split(delim).toString();
+            ItemsMapping[itemIdArr[i]].verificationUrl = verificationUrlArr[i];
+            ItemsMapping[itemIdArr[i]].qrcode = verificationUrlArr[i];
+        }
     }   
     
     function removeItem(string memory itemId) public {
