1fbe57ab289cf4f2b99862a757e85b362da82e7b
vasa <vasa@signy.io>
2020-07-17 17:06:40 +0530

    refactor: add migration scripts and new contract
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@dappkit.io>

--- contracts/Onset.sol
+++ contracts/Onset.sol
@@ -23,12 +23,13 @@ contract Onest {
         string soldLocation;
         string soldToUserId;
         string qrcode;
+        string hash;
     }
     
     mapping (string => Product) public ProductsMapping;
     mapping (string => Item) public ItemsMapping;
     
-    event ItemEvent(string activity, string productId, string itemId);
+    event ItemEvent(string activity, string productId, string itemId, string hash);
     
     function addProduct(string memory productId, string memory brand, string memory name, string memory Type, string memory orgId, string memory attributes) public {
         ProductsMapping[productId] = Product(brand, name, Type, orgId, attributes);
@@ -42,20 +43,25 @@ contract Onest {
         ProductsMapping[productId] = Product(brand, name, Type, orgId, attributes);
     }
     
-    function addItems(string memory itemIds, string memory verificationUrls) public {
+    function addItems(string memory itemIds, string memory verificationUrls, string memory hashes) public {
         strings.slice memory itemIdSlices = itemIds.toSlice();
         strings.slice memory verificationUrlSlices = verificationUrls.toSlice();
+        strings.slice memory hashesSlices = hashes.toSlice();
 
         strings.slice memory delim = ",".toSlice();
         
         string[] memory itemIdArr = new string[](itemIdSlices.count(delim) + 1);
         string[] memory verificationUrlArr = new string[](verificationUrlSlices.count(delim) + 1);
+        string[] memory hashesArr = new string[](hashesSlices.count(delim) + 1);
         
         for(uint i = 0; i < itemIdArr.length; i++) {
             itemIdArr[i] = itemIdSlices.split(delim).toString();
             verificationUrlArr[i] = verificationUrlSlices.split(delim).toString();
+            hashesArr[i] = hashesSlices.split(delim).toString();
+            
             ItemsMapping[itemIdArr[i]].verificationUrl = verificationUrlArr[i];
             ItemsMapping[itemIdArr[i]].qrcode = verificationUrlArr[i];
+            ItemsMapping[itemIdArr[i]].hash = hashesArr[i];
         }
     }   
     
@@ -63,11 +69,11 @@ contract Onest {
         delete ItemsMapping[itemId];
     }
     
-    function updateItem(string memory itemId, uint256 scannedNo, string memory verificationUrl, string memory status, string memory soldTo, string memory soldLocation, string memory soldToUserId, string memory qrcode) public {
-        ItemsMapping[itemId] = Item(scannedNo, verificationUrl, status, soldTo, soldLocation, soldToUserId, qrcode);
+    function updateItem(string memory itemId, uint256 scannedNo, string memory verificationUrl, string memory status, string memory soldTo, string memory soldLocation, string memory soldToUserId, string memory qrcode, string memory hash) public {
+        ItemsMapping[itemId] = Item(scannedNo, verificationUrl, status, soldTo, soldLocation, soldToUserId, qrcode, hash);
     }
     
-    function captureEvent(string memory activity, string memory productId, string memory itemId) public {
-        emit ItemEvent(activity, productId,itemId);
+    function captureEvent(string memory activity, string memory productId, string memory itemId, string memory hash) public {
+        emit ItemEvent(activity, productId,itemId, hash);
     }
 }
---
+++ migrations/2_onest_migration.js
@@ -0,0 +1,5 @@
+const Onest = artifacts.require("Onest");
+
+module.exports = function (deployer) {
+  deployer.deploy(Onest);
+};
