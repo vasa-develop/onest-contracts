2a1694570de7b5729a85fdf54b9ae8a48a612c08
vasa <vasa@signy.io>
2020-10-06 17:09:44 +0530

    refactor: update truffle-config
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@dappkit.io>

--- truffle-config.js
+++ truffle-config.js
@@ -3,9 +3,7 @@ module.exports = {
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "54.163.115.86",
-      //host: "54.180.25.23",
-      //host: "15.165.82.162",
+      host: "IP_ADDRESS",
       port: 22000,
       network_id: "*", // Match any network id
       gasPrice: 0,
